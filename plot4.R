76e9b494328113e123ec436e0be81d9158883104
rberangi <rberangi@gmail.com>
2015-08-10 01:03:44 +1000

    plot4

--- plot4.R
+++ plot4.R
@@ -9,7 +9,7 @@ plot(datetime,  c$Sub_metering_1, type = "l",ylab = "Energy submetering", xlab =
 lines(datetime,  c$Sub_metering_2, type = "l",col="red")
 lines(datetime,  c$Sub_metering_3, type = "l",col="blue")
 
-legend("topright",c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+legend("topright",c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"),bty = "n")
 plot(datetime,  c$Global_reactive_power, type = "l",ylab = "Global_reactive_power", xlab = "datetime",col="black")
 
 
